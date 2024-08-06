ab9f97def45ae858830dbd096dc943e4397fd83f
Júlio Queiroz <jcqaraujo@gmail.com>
2024-08-05 22:08:46 -0400

    atualizações do app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,14 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div>
+    <>
+    <Header />
       <h1>Hello World!</h1>
       <p>Olá Mundo! Estou aprendendo React JS!</p>
-    </div>
+    <Footer/>
+    </>
   );
 }
 
