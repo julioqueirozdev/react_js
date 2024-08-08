cd6f3e0bd8abfd0a65010edf65c6140c8a878b43
Júlio Queiroz <jcqaraujo@gmail.com>
2024-08-08 17:21:56 -0400

    paginas

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,42 +1,10 @@
-import Banner from "./components/Banner";
-import Card from "./components/Card";
-import Category from "./components/Category";
-import Container from "./components/Container";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import videos from './json/videos.json';
+import AppRoutes from './routes';
 
-const categories = [
-  'Geografia',
-  'Como fazer e usar',
-  'Astronomia e Geografia',
-  'Climatologia, Meteorologia, Vegetação',
-  'Geologia e Hidrografia'
-]
 
-function filtercategory(id){
-  return videos.filter(video => video.category === categories[id])
-}
 
 function App() {
   return (
-    <>
-    <Header />
-    <Banner image="home" />
-    <Container>
-      
-    {
-      categories.map((category, index)=> 
-        <Category category={category}>
-      {filtercategory(index).map((video) => {
-          return <Card id={video.id} key={video.id}/> })}
-      </Category>
-      )
-    }
-
-    </Container>
-    <Footer/>
-    </>
+    <AppRoutes />
   );
 }
 
---
+++ my-app/src/pages/Home/index.js
@@ -0,0 +1,30 @@
+import Banner from "../../components/Banner";
+import Card from "../../components/Card";
+import Category, {categories, filtercategory} from "../../components/Category";
+import Container from "../../components/Container";
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
+
+
+
+function Home() {
+  return (
+    <>
+    <Header />
+    <Banner image="home" />
+    <Container>    
+    {
+      categories.map((category, index)=> 
+        <Category category={category}>
+      {filtercategory(index).map((video) => {
+          return <Card id={video.id} key={video.id}/> })}
+      </Category>
+      )
+    }
+    </Container>
+    <Footer/>
+    </>
+  );
+}
+
+export default Home;
