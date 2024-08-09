68d32655a0e0a3f4a7b4563c4e930ebc4cbffa2c
Júlio Queiroz <jcqaraujo@gmail.com>
2024-08-09 14:42:35 -0400

    page 404

--- my-app/src/routes.js
+++ my-app/src/routes.js
@@ -1,6 +1,7 @@
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Home from './pages/Home';
 import Watch from './pages/Watch';
+import PageNotFound from './pages/PageNotFound';
 
 function AppRoutes(){
     return(
@@ -8,6 +9,8 @@ function AppRoutes(){
             <Routes>
                 <Route path='/' element={<Home/>}></Route>
                 <Route path='/watch' element={<Watch/>}></Route>
+                <Route path='*' element={<PageNotFound/>}></Route>
+                {/* o * é o seletor global. qualquer acesso a paginas que não sejam / ou /watch receberá a page not found */}
             </Routes>
         </BrowserRouter>
     );
