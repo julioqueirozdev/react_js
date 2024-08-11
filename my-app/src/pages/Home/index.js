894390b5cd30a5543ea0148380f30693c14e51c8
Júlio Queiroz <jcqaraujo@gmail.com>
2024-08-10 21:34:46 -0400

    React hooks

--- my-app/src/pages/Home/index.js
+++ my-app/src/pages/Home/index.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Banner from "../../components/Banner";
 import Card from "../../components/Card";
 import Category, {categories, filtercategory} from "../../components/Category";
@@ -8,19 +9,40 @@ import Header from "../../components/Header";
 
 
 function Home() {
+
+  //let nomeDigitado = ""
+  function pegarNome(event){
+    console.log(event.target.value)
+    //nomeDigitado = event.target.value
+    setNome(event.target.value)
+  }
+
+  //Hook = gancho -> useState
+
+  const [nome, setNome] = useState();
+
   return (
     <>
     <Header />
     <Banner image="home" />
     <Container>    
-    {
-      categories.map((category, index)=> 
+    
+      <input
+      type= 'text'
+      placeholder="Nome"
+      // value='' impede que seja digitado texto na caixa de texto
+      onChange={pegarNome}
+      />
+
+      <h2>{nome}</h2>
+
+    {/* { categories.map((category, index)=> 
         <Category category={category}>
       {filtercategory(index).map((video) => {
           return <Card id={video.id} key={video.id}/> })}
       </Category>
-      )
-    }
+      )} */}
+
     </Container>
     <Footer/>
     </>
