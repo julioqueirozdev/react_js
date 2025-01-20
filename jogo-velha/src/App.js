0a7740e5b9816eb70b97c429dd29459f5e2298a8
Júlio Queiroz <jcqaraujo@gmail.com>
2025-01-19 22:06:33 -0400

    doc: refatoração do board com loops nos squares

--- jogo-velha/src/App.js
+++ jogo-velha/src/App.js
@@ -12,6 +12,13 @@ function Square({value, onSquareClick}){
 
 
 function Board({xIsNext, squares, onPlay}){
+  const renderSquare = (i) =>{
+    return(
+      <Square value={squares[i]} onSquareClick={()=> handleClick(i)}/>
+    );
+  }
+  
+  
   function handleClick(i){
     if(calculateWinner(squares) || squares[i]){
       return;
@@ -37,20 +44,15 @@ function Board({xIsNext, squares, onPlay}){
   return(
     <>
     <div className='status'>{status}</div>
-    <div className="board-row">
-      <Square value={squares[0]} onSquareClick={()=> handleClick(0)}/>
-      <Square value={squares[1]} onSquareClick={()=> handleClick(1)}/>
-      <Square value={squares[2]} onSquareClick={()=> handleClick(2)}/>
-    </div>
-    <div className="board-row">
-      <Square value={squares[3]} onSquareClick={()=> handleClick(3)}/>
-      <Square value={squares[4]} onSquareClick={()=> handleClick(4)}/>
-      <Square value={squares[5]} onSquareClick={()=> handleClick(5)}/>
-    </div>
-    <div className="board-row">
-      <Square value={squares[6]} onSquareClick={()=> handleClick(6)}/>
-      <Square value={squares[7]} onSquareClick={()=> handleClick(7)}/>
-      <Square value={squares[8]} onSquareClick={()=> handleClick(8)}/>
+    <div>
+      {
+        Array(3).fill(null).map((_, row) => (<div className='board-row' key={row}>
+          {Array(3).fill(null).map((_, col) => {
+            const index = row * 3 + col;
+            return renderSquare(index)           
+          })}
+        </div>
+      ))};
     </div>
     </>
   );
